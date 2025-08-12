d52de82a373bed7c83fb3e1c733dd6cdca821868
wizard <lenkgit@gmail.com>
2025-08-12 14:23:31 -0400

    chore(environment): remove unused

--- src/main/kotlin/com/ebay/oauth2/data/Environment.kt
+++ src/main/kotlin/com/ebay/oauth2/data/Environment.kt
@@ -1,6 +1,6 @@
 package com.ebay.oauth2.data
 
-enum class Environment(val identifier: String, val webEndpoint: String, val apiEndpoint: String) {
-    PRODUCTION("api.ebay.com", "https://auth.ebay.com/oauth2/authorize", "https://api.ebay.com/identity/v1/oauth2/token/"),
-    SANDBOX("api.sandbox.ebay.com", "https://auth.sandbox.ebay.com/oauth2/authorize", "https://api.sandbox.ebay.com/identity/v1/oauth2/token/");
+enum class Environment(val webEndpoint: String, val apiEndpoint: String) {
+    PRODUCTION("https://auth.ebay.com/oauth2/authorize", "https://api.ebay.com/identity/v1/oauth2/token/"),
+    SANDBOX("https://auth.sandbox.ebay.com/oauth2/authorize", "https://api.sandbox.ebay.com/identity/v1/oauth2/token/");
 }
